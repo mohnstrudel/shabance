8cc8eb85d5be9f8b99ef5e622ab6ec21a1c90c1b
Anton Kostin <schnibba@gmail.com>
2018-05-31 16:23:50 +0300

    Add webpacker.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( admin.css admin.js front.css front.js )
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
